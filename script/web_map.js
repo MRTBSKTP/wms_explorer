3967bd016073eb2761d511c7a9cf1a4fdf83a442
MRTBSKTP <mert5iktepe@gmail.com>
2021-03-06 23:58:31 +0300

    recreated form before previous repo break

--- script/web_map.js
+++ script/web_map.js
@@ -2,3 +2,10 @@
     Code and logic concerning the web map on the page will reside here, Since we are
     pulling it from a CDN Leaflet will use defalut alias 'L' in global scope!.
 */
+
+// For mockup purposes will use node module approach...
+let map = L.map('map').setView([0, 0], 2);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
